3d36375d684f5d3638725b8c395199e6f992cc70
gregoryferreira09 <gregory_ferreira@outlook.fr>
2025-06-17 13:58:25 +0200

    Update gameflow.js

--- SRC/JS/gameflow.js
+++ SRC/JS/gameflow.js
@@ -1,5 +1,5 @@
 window.parcoursEquipes = [
-  [1, 2, 5, 3, 4], // Equipe 1
+  [5, 2, 5, 3, 4], // Equipe 1
   [2, 4, 3, 5, 1], // Equipe 2
   [3, 1, 4, 2, 5], // Equipe 3
   [4, 5, 2, 1, 3], // Equipe 4
