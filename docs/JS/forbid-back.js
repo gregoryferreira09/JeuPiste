343c4345587f4f7e83b8771687594c4d3901c312
gregoryferreira09 <gregory_ferreira@outlook.fr>
2025-06-20 14:14:30 +0200

    Update forbid-back.js

--- docs/JS/forbid-back.js
+++ docs/JS/forbid-back.js
@@ -1,7 +1,12 @@
 (function() {
-  history.pushState(null, null, location.href);
-  window.onpopstate = function () {
-    history.pushState(null, null, location.href);
-    alert("La navigation avec le bouton retour est désactivée.\nMerci d'utiliser les boutons du site.");
+  // On pousse 2 états pour piéger le bouton retour
+  history.pushState({ page: 1 }, "", "");
+  history.pushState({ page: 2 }, "", "");
+
+  window.onpopstate = function(event) {
+    if (event.state && event.state.page === 1) {
+      history.pushState({ page: 2 }, "", "");
+      alert("La navigation avec le bouton retour est désactivée.\nMerci d'utiliser les boutons du site.");
+    }
   };
 })();
