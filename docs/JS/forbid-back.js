b364d81d58fe7bc88bd267ae674071db83ab0d23
gregoryferreira09 <gregory_ferreira@outlook.fr>
2025-06-20 14:33:03 +0200

    Update forbid-back.js

--- docs/JS/forbid-back.js
+++ docs/JS/forbid-back.js
@@ -1,8 +1,10 @@
+// forbid-back.js
 (function() {
-  // Permet de désactiver la protection lors de navigation programmée
+  // Permet de désactiver la protection lors de navigation programmée (ex : bouton du jeu)
   let blockBack = true;
   window.disableBackProtection = function() { blockBack = false; };
 
+  // Bloque le retour arrière du navigateur (flèche, swipe, bouton retour mobile)
   history.pushState(null, null, location.href);
   window.onpopstate = function () {
     if (blockBack) {
@@ -10,4 +12,13 @@
       alert("Vous ne pouvez pas quitter la partie en utilisant le bouton retour. Merci d'utiliser les boutons du jeu.");
     }
   };
+
+  // Si tu veux aussi bloquer la fermeture/refresh, laisse ce bloc (optionnel) :
+  // window.onbeforeunload = function(e) {
+  //   if (blockBack) {
+  //     e.preventDefault();
+  //     e.returnValue = "Êtes-vous sûr de vouloir quitter la partie ? Vous risquez d’être déconnecté(e) du jeu.";
+  //     return "Êtes-vous sûr de vouloir quitter la partie ? Vous risquez d’être déconnecté(e) du jeu.";
+  //   }
+  // };
 })();
