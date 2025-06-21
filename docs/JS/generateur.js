2c5ab4a2a346e41366a2169625e15427d15e571e
gregoryferreira09 <gregory_ferreira@outlook.fr>
2025-06-21 11:44:42 +0200

    Update generateur.js

--- docs/JS/generateur.js
+++ docs/JS/generateur.js
@@ -359,6 +359,11 @@ function initLeafletMap(targetInput) {
 
   // 3. Crée la nouvelle carte sur le container tout neuf
   window.map = L.map('mapContainer').setView([48.858370, 2.294481], 13);
+  ...
+}
+
+  // 3. Crée la nouvelle carte sur le container tout neuf
+  window.map = L.map('mapContainer').setView([48.858370, 2.294481], 13);
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap'
   }).addTo(window.map);
