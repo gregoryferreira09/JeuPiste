c0fc2078226b19e47cfc8776fb01c027d1733eb2
gregoryferreira09 <gregory_ferreira@outlook.fr>
2025-06-23 23:49:49 +0200

    Update catalogue.js

--- docs/JS/catalogue.js
+++ docs/JS/catalogue.js
@@ -275,8 +275,8 @@ const QUESTS_CATALOGUE = [
   {
   id: "photo_inconnus",
   nom: "Photo avec des inconnus",
-  description: "Prendre une ou plusieurs photos avec des personnes différentes ou répondant à un critère déterminé. (Qui porte des lunettes ; qui a plus de 50 ans ; qui porte une chemise blanche, etc.)",
-  parametres: [
+description: "Prendre une ou plusieurs photos avec des personnes différentes ou répondant à un critère déterminé. (Qui porte des lunettes ; qui a plus de 50 ans ; qui porte une chemise blanche, etc.)",
+parametres: [
     { key: "nbPersonnes", type: "number", label: "Nombre de personnes/photos", default: 1, min: 1, max: 10 },
     { key: "critere", type: "text", label: "Critère ou consigne", placeholder: "ex : Personne qui porte un chapeau" }
   ],
