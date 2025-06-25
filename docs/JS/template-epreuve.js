3f74f0b995c0aab50f5eddc5e419759b3ff3997a
gregoryferreira09 <gregory_ferreira@outlook.fr>
2025-06-25 13:20:18 +0200

    Update template-epreuve.js

--- docs/JS/template-epreuve.js
+++ docs/JS/template-epreuve.js
@@ -71,6 +71,7 @@ function showToast(msg) {
 // Fonction utilitaire pour joindre une liste au format "A et B", "A, B et C"
 function joinList(arr) {
   if (!Array.isArray(arr)) return arr;
+  const lower = x => x ? x.charAt(0).toLowerCase() + x.slice(1) : x;
   if (arr.length === 1) return arr[0];
   if (arr.length === 2) return arr[0] + ' et ' + arr[1];
   return arr.slice(0, -1).join(', ') + ' et ' + arr[arr.length - 1];
